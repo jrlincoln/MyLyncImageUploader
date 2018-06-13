228765fb6aa69273f410e3a2656bc296d50f591b
Jacob Lincoln <jake@jakelincoln.com>
2018-06-13 09:11:52 -0500

    Initial Commit

---
+++ MyLyncImageUploader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyLyncImageUploader
+//
+//  Created by Jake on 6/13/18.
+//  Copyright © 2018 LyncoProducts. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
